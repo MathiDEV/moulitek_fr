4c49089f2b9592e79de4bd527e4c758b9d374d70
Mathias A <mathias.andre@epitech.eu>
2022-03-24 07:12:29 +0100

    [FIX] Fixed lines badges.

--- trace.php
+++ trace.php
@@ -175,7 +175,7 @@ foreach ($coding_style["major"]["list"] as $code => $major) {
                             <th scope="col">Manquantes</th>
                             </tr>
                         </thead>
-                        <tbody>';
+                        <tbody class="border-top-0">';
                         foreach($coverage_data as $data) {
                             echo '<tr>
                             <th scope="row">'.$data["file"].'</th>

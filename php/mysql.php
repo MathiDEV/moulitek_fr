8318b91e26c6a7c4da4485dd756a8fa0a88c04a0
Mathias A <mathias.andre@epitech.eu>
2022-03-24 05:18:31 +0100

    [OTHER] Debug mode.

--- php/mysql.php
+++ php/mysql.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
 $mysql = mysqli_connect(getenv("DB_HOST"), getenv("DB_USER"), getenv("DB_PASS"), getenv("DB_NAME"));
 if (!$mysql) {
     exit;
